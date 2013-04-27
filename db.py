c810eda29e1e82ebbc7c881a98a858f3ad6f8293
sbabski <bryan@ubuntu.(none)>
2013-04-27 11:52:25 -0400

    added more images

--- db.py
+++ db.py
@@ -47,13 +47,13 @@ def addCurrentTourtoUser(username, title):
         if user["username"] == username:
             db.users.update({"username":username},{"username":username, "accesskey":user["accesskey"], "tours":user["tours"], "points":user["points"], "currenttour":title, "currenttourstatus":"Begin"})
 
-
-def __init__():
+def __int__():
     addUser("swyetzner","38472")
 # addUser("sbabski","62398")
 # print getUserList()
     addTour("New York Hipster Tour", "a hipster tour", ["Go here","Go there","Go back here"], ["stop being stupid","yes"], [5,4,3,2,1], 1231245.343, "this was bad",  "New York City", "http://www.newyorkpersonalinjuryattorneyblog.com/wp-content/uploads/2010/06/NewYork.jpg")
     addTour("New York Not Hipster Tour", "a not hipster tour", ["Go here","Go there","Go back here"], ["stop being stupid","yes"], [5,4,3,2,1], 1231245.343, "this was bad",  "New York City", "http://www.sfexaminer.com/files/blog_images/New%20York%20skyline_0.jpg")
+    addTour("New York Normal Tour", "a not hipster tour", ["Go here","Go there","Go back here"], ["stop being stupid","yes"], [5,4,3,2,1], 1231245.343, "this was bad",  "New York City", "http://www.drug-rehab.org/wp-content/uploads/2011/03/New-York-2.jpg")
     addTour("Washington Hipster Tour", "a hipster tour", ["Go here","Go there","Go back here"], ["stop being stupid","yes"], [5,4,3,2,1], 1231245.343, "this was bad",  "Washington D.C.", "img1")
     addCurrentTourtoUser("swyetzner","New York Hipster Tour")
     print getUser("swyetzner")
