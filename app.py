8ddc983d056cdb527a0d70029c71cc5a9ece6e9c
Sofia Wyetzner <sofia.wyetzner@gmail.com>
2013-06-20 14:53:13 -0400

    fixed city data stuff

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def create():
         title = request.form["title"]
         description = request.form["description"]
         image = request.form["image"]
-        city = request.form["city"]
+        city = request.form["selectedCity"]
         clues = []
         hints = []
         ratings = []
